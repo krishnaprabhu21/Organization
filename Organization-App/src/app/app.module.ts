153f3f62a78aef7511bc58e08b6e5923b6d1dbf5
Ivo Costa <costa_ivo@yahoo.com>
2017-07-14 16:43:35 +0530

    Added Member Component & Service

--- Organization-App/src/app/app.module.ts
+++ Organization-App/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MemberListComponent } from './member-list/member-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    MemberListComponent
   ],
   imports: [
     BrowserModule
