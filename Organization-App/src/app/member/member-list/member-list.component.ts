3b75ed41610c304193c6230e01699001a26b6c4e
CREATIVECAPSULE\aamod.talauliker <aamod.talauliker@creativecapsule.com>
2017-07-20 12:46:17 +0530

    done with fetching data from api

--- Organization-App/src/app/member/member-list/member-list.component.ts
+++ Organization-App/src/app/member/member-list/member-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from './member.service';
+import { IMembers } from './members';
 
 @Component({
   selector: 'app-member-list',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemberListComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = "Members";
+  Members: IMembers[];
+  errorMessage: string;
+
+  constructor(private _MemberService: MemberService) {
+
+  }
 
   ngOnInit() {
+    // this._MemberService.getAll()
+    //     .subscribe(Members => this.Members = Members,
+    //     error => this.errorMessage = <any>error);
+
+    this._MemberService.getAll().subscribe(
+      data => {
+        this.Members = data;
+        console.log("Members: ", this.Members)
+      },
+      error => {
+        console.log("Members: user details, ", error)
+      },
+      () => {
+      })
   }
 
 }
---
+++ Organization-App/src/app/shared/right-navbar/right-bottom-navbar/right-bottom-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right-bottom-navbar',
+  templateUrl: './right-bottom-navbar.component.html',
+  styleUrls: ['./right-bottom-navbar.component.css']
+})
+export class RightBottomNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
